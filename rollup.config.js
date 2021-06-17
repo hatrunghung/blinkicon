9e4306a6280f090c26bad6bcad5b4c80b7324431
Hung Ha Trung <hungtro96@gmail.com>
2021-06-18 03:09:29 +1000

    add rollup build config

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import multi from '@rollup/plugin-multi-entry'
+import babel from '@rollup/plugin-babel'
+
+const formats = ['esm', 'umd'] // cjs?
+
+export default {
+	input: ['src/16/index.js', 'src/24/index.js'],
+	output: formats.map(format => ({
+		file: `dist/index.${format}.js`,
+		format,
+		name: 'blinkicon',
+		globals: {
+			'@babel/runtime/helpers/extends': '_extends',
+			'@babel/runtime/helpers/objectWithoutProperties': '_objectWithoutProperties',
+			'react': 'React'
+		}
+	})),
+	external: [/@babel\/runtime/, 'react'],
+	plugins: [
+		multi(),
+		babel({
+			babelHelpers: 'runtime',
+			plugins: ["@babel/plugin-transform-runtime"]
+		})
+	]
+}
